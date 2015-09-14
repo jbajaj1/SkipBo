f9d1807e3964243ef6d4152082fc445d94117c1d
Achintya Gopal <achintyagopal@macbook.local>
2015-09-13 23:30:46 -0400

    Remove personal info

--- DiscardPile.hpp
+++ DiscardPile.hpp
@@ -1,28 +1,3 @@
-/*
-Paul Watson
-pwatso14
-pwatso14@jhu.edu
-443-812-2302
-
-Achintya Gopal
-agopal2
-agopal2@jhu.edu
-520-508-1337
-
-William Watson
-wwatso13
-wwatso13@jhu.edu
-516-543-1131
-
-Joshan Bajaj
-jbajaj1
-jbajaj1@jhu.edu
-609-649-1214
-
-Team 2A
-600.120
-Final Project
-*/
 #ifndef DISCARDPILE_HPP
 #define DISCARDPILE_HPP
 
